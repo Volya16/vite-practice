0a0e77e51e9e446874c4f9557a37c8e2c461084a
Larysa <seagull2610@gmail.com>
2025-01-11 19:20:50 +0100

    promise

--- src/main.js
+++ src/main.js
@@ -16,51 +16,76 @@ const USER_DATA = {
 
 const localStorageKey = 'user-data';
 
-
 const loginFormEl = document.querySelector('.login-form');
 const btnEl = document.querySelector('[name="button"]');
 
 const { email, password } = loginFormEl.elements;
 
+loginFormEl.addEventListener('submit', evt => {
+  evt.preventDefault();
+  if (btnEl.textContent === 'Logout') {
+    localStorage.removeItem(localStorageKey);
+    loginFormEl.reset();
+    email.removeAttribute('readonly');
+    password.removeAttribute('readonly');
+    btnEl.textContent = 'Login';
+    return;
+  }
 
-loginFormEl.addEventListener('submit', (evt) => {
-    evt.preventDefault();
-    if (btnEl.textContent === 'Logout') {
-        localStorage.removeItem(localStorageKey)   
-        loginFormEl.reset();
-        email.removeAttribute('readonly');
-        password.removeAttribute('readonly');
-        btnEl.textContent = 'Login';
-        return;
-    }
+  const emailValue = email.value.trim();
+  const passwordValue = password.value.trim();
 
-    const emailValue = email.value.trim();
-    const passwordValue = password.value.trim();
-    
-    if (emailValue === "" || passwordValue === "") {
-        alert('Fill all fields!');
-        return;
-    } 
-    if (emailValue !== USER_DATA.email || passwordValue !== USER_DATA.password) {
-        alert('Wrong data!');
-        return;
-    }
-    localStorage.setItem(localStorageKey, JSON.stringify({ email: emailValue, password: passwordValue }));
-    
-    btnEl.textContent = 'Logout';
-    email.setAttribute('readonly', true);
-    password.setAttribute('readonly', true);
-})
+  if (emailValue === '' || passwordValue === '') {
+    alert('Fill all fields!');
+    return;
+  }
+  if (emailValue !== USER_DATA.email || passwordValue !== USER_DATA.password) {
+    alert('Wrong data!');
+    return;
+  }
+  localStorage.setItem(
+    localStorageKey,
+    JSON.stringify({ email: emailValue, password: passwordValue })
+  );
+
+  btnEl.textContent = 'Logout';
+  email.setAttribute('readonly', true);
+  password.setAttribute('readonly', true);
+});
 
 const savedData = localStorage.getItem(localStorageKey);
 if (savedData) {
-    const parsedData = JSON.parse(savedData)
-    email.value = parsedData.email;
-    password.value = parsedData.password;
-    btnEl.textContent = 'Logout';
-    email.setAttribute('readonly', true);
-    password.setAttribute('readonly', true);
+  const parsedData = JSON.parse(savedData);
+  email.value = parsedData.email;
+  password.value = parsedData.password;
+  btnEl.textContent = 'Logout';
+  email.setAttribute('readonly', true);
+  password.setAttribute('readonly', true);
 }
 
+// Додай відображення дати і часу в реальному часі
 
+const dateTime = document.querySelector('.date span');
 
+dateTime.textContent = new Date().toLocaleString();
+
+setInterval(() => {
+  dateTime.textContent = new Date().toLocaleString();
+}, 1000);
+
+// Перероби функцію на проміс таким чином, щоб проміс повертав значення
+// через 2 секунди після виклику функції
+
+function greet() {
+  return 'hello world';
+}
+
+const promise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve('hello world');
+  }, 2000);
+});
+
+promise.then(response => {
+  console.log(response);
+});
