47da93cc987317c24de58beaf3d3c7fccf1593d6
VitaliiZvieriev <zverew91@gmail.com>
2025-01-11 20:55:10 +0200

    add res last

--- src/main.js
+++ src/main.js
@@ -87,5 +87,61 @@ const promise = new Promise((resolve, reject) => {
 });
 
 promise.then(response => {
-  console.log(response);
+  //   console.log(response);
 });
+
+// - Використовуй prompt та повертай значення звідти.
+// - Створи функцію, яка буде набувати значення з prompt і повертатиме проміс.
+// Якщо значення не є числом, відхиляй проміс та логіруй "error".
+// Якщо значення парне, вирішуй проміс та повертай "even" через 1 секунду.
+// Якщо значення не парне, вирішуй проміс та повертай "odd" через 2 секунди.
+
+// const userData = prompt('Enter somthing!');
+// fun(userData)
+//   .then(res => {
+//     console.log(res);
+//   })
+//   .catch(err => console.log(err));
+
+function fun(data) {
+  return new Promise((res, rej) => {
+    if (Number.isNaN(Number(data))) {
+      rej('error');
+    } else if (data % 2 === 0) {
+      setTimeout(() => {
+        res('even');
+      }, 1000);
+    } else {
+      setTimeout(() => {
+        res('odd');
+      }, 2000);
+    }
+  });
+}
+
+// Напишіть функцію calculateAge(birthDate), яка приймає дату народження у форматі YYYY-MM-DD і повертає поточний вік.
+// Підказка: Використайте об'єкт Date для обчислення різниці між сьогоднішньою датою і датою народження
+
+const calculateAge = birthDate => {
+  const userDate = new Date(birthDate);
+
+  const birthdayYear = userDate.getFullYear();
+  const birthdayMonth = userDate.getMonth();
+  const birthdayDay = userDate.getDate();
+
+  let diffYear = new Date().getFullYear() - birthdayYear;
+
+  const diffMouth = new Date().getMonth() - birthdayMonth;
+
+  const diffDay = new Date().getDate() - birthdayDay;
+
+  if (diffMouth < 0 || (diffDay < 0 && diffMouth === 0)) {
+    diffYear -= 1;
+  }
+
+  return diffYear;
+};
+
+console.log(calculateAge('2000-01-07'));
+console.log(calculateAge('2000-01-17'));
+console.log(calculateAge('2000-07-17'));
